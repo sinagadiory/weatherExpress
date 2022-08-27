051c29fdc4ddd142506088d25ce991eeddc171e0
dioripribadisinaga@gmail.com <dioripribadisinaga@gmail.com>
2022-08-27 12:14:25 +0700

    weather

--- config/router.js
+++ config/router.js
@@ -4,13 +4,18 @@ const router = express.Router()
 const { weather } = require("../app/controller")
 const convert = require("../app/convertSuhu")
 
-
+const Kembalikan = (callback) => {
+    setTimeout(() => {
+        callback(fs.writeFileSync("app/models/history.json", "[]"))
+    }, 500)
+}
 
 router.get("/suhu", weather.getOneCity)
 
-router.get("/", (req, res) => {
-    fs.writeFileSync("app/models/history.json", "[]")
-    res.render("index", { weather: null, convert, history: null, err: null })
+router.get("/", async (req, res) => {
+    Kembalikan((result) => {
+        res.render("index", { weather: null, convert, history: null, err: null })
+    })
 })
 
 router.post("/search", weather.GetWeather)
